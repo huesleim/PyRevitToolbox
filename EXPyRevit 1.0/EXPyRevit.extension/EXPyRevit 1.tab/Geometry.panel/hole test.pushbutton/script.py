0900725d151464ec85b5e6511506410fe8b5261a
Hueslei <234298003+huesleim@users.noreply.github.com>
2026-07-22 10:09:45 -0300

    Refactoring of separate floors script to consider floors with holes; separating concerns by function

--- EXPyRevit 1.0/EXPyRevit.extension/EXPyRevit 1.tab/Geometry.panel/hole test.pushbutton/script.py
+++ EXPyRevit 1.0/EXPyRevit.extension/EXPyRevit 1.tab/Geometry.panel/hole test.pushbutton/script.py
@@ -22,6 +22,16 @@ def pick_floor():
     )
     return doc.GetElement(ref.ElementId)
 
+def area_calc(points):
+    n = len(points)
+    area = 0.0
+    for i in range(n):
+        x0, y0 = points[i].X, points[i].Y
+        x1, y1 = points[(i+1) % n].X, points[(i+1) % n].Y
+        area += (x0 * y1) - (x1 * y0)
+    return abs(area) / 2
+
+
 def process_floors(floor):
     floor_type_id = floor.GetTypeId()
     flor_level_id = floor.LevelId
@@ -40,112 +50,101 @@ def process_floors(floor):
 
         for j, curve in enumerate(curve_array):
             tessellated_points = curve.Tessellate()
-            points_list.append(tessellated_points)
+            points_list.extend(tessellated_points)
         
         curve_array_dict["points"] = points_list
+        curve_array_dict["area"] = area_calc(points_list)
         curve_array_objects_dict[i] = curve_array_dict
     
     return curve_array_objects_dict
 
-def search_cache(point_array, curve_array_objects_dict):
+def point_in_polygon_points(point, polygon_points):
+    x, y = point.X, point.Y
+    n = len(polygon_points)
+    k = n-1
 
+    inside = False
 
+    for l in range(n):
+        xl, yl = polygon_points[l].X, polygon_points[l].Y
+        xk, yk = polygon_points[k].X, polygon_points[k].Y
 
-def point_in_polygon_points(curve_array_objects_dict):
-    for i, element in enumerate(curve_array_objects_dict):
-        for j, other_element in enumerate(curve_array_objects_dict):
-            if i == j: 
+        intersects = ((yl > y) != (yk > y)) and (
+            x < (xk - xl) * (y - yl) / (yk - yl) + xl
+        )
+
+        if intersects:
+            inside = not inside
+        
+        k = l
+        
+    return inside
+
+def compare_all_polygons(curve_array_objects_dict):
+    polygon_relationship = {}
+    for i in curve_array_objects_dict:
+        polygon_parents = []
+
+        for j in curve_array_objects_dict:
+            if i == j:
                 continue
-            
-            x, y = element[i]["points"][0].X, element[i]["points"][0].Y
-            
+            point = curve_array_objects_dict[i]["points"][0]
+            polygon_points = curve_array_objects_dict[j]["points"]
+            result = point_in_polygon_points(point, polygon_points)
 
-            inside = False
-            n = len(curve_array_objects_dict[j]["points"])
-            k = n-1
+            if result: polygon_parents.append(j)
+        polygon_relationship[i] = polygon_parents
+    return polygon_relationship
 
-            for l in range(n):
-                xk, yk = element[l]["points"][0].X, element[l]["points"][0].Y
-                xk, yk = polygon_points[k].X, polygon_points[k].Y
-                xj, yj = polygon_points[l].X, polygon_points[l].Y
-                intersects = ((yi > y) != (yj > y)) and (
-                    x < (xj - xi) * (y - yi) / (yj - yi) + xi
-                )
+def resolve_parentship(polygon_relationship, curve_array_objects_dict):
+    curves_output = []
 
-                if intersects:
-                    inside = not inside
+    for i in polygon_relationship:
+        merged_curves = []
+        smallest_area = float("inf")
+        closest_parent = None
 
-                j = i
+        if not polygon_relationship[i]:
+            continue
 
-            return inside
-
-
-def create_floors
-
-
-
-# curve_output = []
-
-# try: 
-    
-#     floor = pick_floor()
-#     if floor:
-#         floor_type_id = floor.GetTypeId()
-#         floor_level_id = floor.LevelId
-#         sketch = doc.GetElement(floor.SketchId)
-
-#         floor_input = []
-#         floor_output = []
-#         inside = False
-
-#         #populating floor_input with curves and points
-#         for loop_index, profile_loop in enumerate(sketch.Profile):
-#             curves = []
-#             points = []
-
-#             for curve in profile_loop:
-#                 tessellated_points = curve.Tessellate()
-#                 points.extend(tessellated_points)
-#                 curves.append(curve)
-
-#             dict_item = {}
-#             dict_item["curves"] = curves
-#             dict_item["points"] = points 
-#             floor_input.append(dict_item)
+        for j in polygon_relationship[i]:
+            area_j = curve_array_objects_dict[j]["area"] 
+            if area_j < smallest_area:
+                smallest_area: area_j
+                closest_parent = j
         
-#         n = len(floor_input)
+        if closest_parent is not None:
+            merged_curves.extend(curve_array_objects_dict[closest_parent]["curves"])
+        merged_curves.extend(curve_array_objects_dict[i]["curves"])
 
-#         #comparing every loop looking for parent loops
-#         for i, dict_item in enumerate(floor_input):
-#             for j, comparison_item in enumerate(floor_input):
-#                 if i == j:
-#                     continue
-#                 if point_in_polygon_points(dict_item["points"][0], comparison_item["points"]):
-#                     loop = []
-#                     loop.append(dict_item["curves"])
-#                     loop.append(comparison_item["curves"])
-#                     floor_output.append(loop)
-#                     break
-#                 floor_output.append(dict_item["curves"])
-        
+        curves_output.append(merged_curves)
+
+    return curves_output
 
 
-#         for sketch in floor_output: 
-#             cl = CurveLoop()
-#             for c in sketch:
-#                 cl.Append(c)
-#             Floor.Create(
-#                 doc,
-#                 floor,
-#                 floor.GetTypeId(),
-#                 floor.LevelId
-#             )
-#         doc.Delete(floor.Id)
+def create_floors (merged_curves, floor):
+    try:
+        t = Transaction(doc, "Separate floor")
+        t.Start()
+
+        for curve_array in merged_curves:
+            cl = CurveLoop()
+            for curve in curve_array:
+                cl.Append(c)
+
+
+            Floor.Create(
+                doc,
+                [cl],
+                floor.GetTypeId(),
+                floor.LevelId
+            )
+        doc.Delete(floor.Id)
+
+        t.Commit()
             
-            
+    except OperationCanceledException:
+        print("Selection cancelled.")
 
-except OperationCanceledException:
-    print("Selection cancelled.")
-
-except Exception:
-    traceback.print_exc()
+    except Exception:
+        traceback.print_exc()
