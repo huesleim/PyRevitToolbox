79df5bc2796aaa47e6ca116db55316ed3c6425b8
Hueslei Miranda <234298003+huesleim@users.noreply.github.com>
2026-07-28 17:53:31 -0300

    Add quick script for a renumber sheet automation
    WIP

---
+++ EXPyRevit 1.0/EXPyRevit.extension/EXPyRevit 1.tab/Parameter.panel/RENUMERAR.pushbutton/script.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from Autodesk.Revit.DB import Transaction, ViewSheet
+from pyrevit import forms
+import traceback
+
+uidoc = __revit__.ActiveUIDocument
+doc = uidoc.Document
+
+
+def pick_sheets():
+    return [
+        e for e in
+        (doc.GetElement(id) for id in uidoc.Selection.GetElementIds())
+        if isinstance(e, ViewSheet)
+    ]
+
+
+try:
+    sheets = pick_sheets()
+
+    if not sheets:
+        forms.alert("Please pre-select one or more sheets in the Project Browser.")
+        raise Exception("No sheets selected.")
+
+    starting_number = forms.ask_for_string(
+        prompt="Número inicial",
+        default="0001"
+    )
+
+    if not starting_number:
+        raise Exception("Operation cancelled.")
+
+    width = len(starting_number)
+
+    t = Transaction(doc, "Renumber sheets")
+    t.Start()
+
+    for i, sheet in enumerate(sheets):
+        sheet.SheetNumber = str(int(starting_number) + i).zfill(width)
+
+    t.Commit()
+
+except Exception:
+    traceback.print_exc()
